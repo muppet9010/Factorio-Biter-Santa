f2c3d682411ad4a6a53c9fbf74e89f9b19c90a06
muppet9010 <40836017+muppet9010@users.noreply.github.com>
2020-11-28 17:29:25 +0000

    utility update

--- control.lua
+++ control.lua
@@ -1,6 +1,5 @@
 local Santa = require("scripts/santa")
 local SantaActivity = require("scripts/santa_activity")
-local Events = require("utility/events")
 
 local OnLoad = function()
     Santa.OnLoad()
@@ -14,4 +13,3 @@ end
 script.on_init(OnStartup)
 script.on_load(OnLoad)
 script.on_configuration_changed(OnStartup)
-Events.RegisterEvent(defines.events.on_tick)
